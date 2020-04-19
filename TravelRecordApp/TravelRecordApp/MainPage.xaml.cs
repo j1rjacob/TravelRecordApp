ff667cfa3bb3ce61dc8e47262b7ec96bb93b56c3
junar <junarjacob@yahoo.com>
2020-04-19 17:50:44 +0300

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            var isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            var isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if(isEmailEmpty || isPasswordEmpty)
+            {
+
+            }
+            else
+            {               
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewTravelPage : ContentPage
+	{
+		public NewTravelPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
